58c9640313f9d28a0920c6267b6da9f03b0b29fc
mango-days <positivejasminekaur@gmail.com>
2021-11-04 22:50:57 +0530

    does not work for large sized input=>stops midway

---
+++ n_sized_combinations_in_an_array.py
@@ -0,0 +1,16 @@
+array=[1,2,3,4,5,6,7,8,9,0]
+visited=[0,0,0,0,0,0,0,0,0,0]
+size=3
+ans=[]
+def combination():
+    if len(ans)==size:
+        print (ans)
+        return
+    for i in range (0, len(array)):
+        if visited[i]==0:
+            visited[i]=1
+            ans.append(array[i])
+            combination()
+            ans.pop()
+            visited[i]=0
+combination()
